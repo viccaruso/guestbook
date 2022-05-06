e044acf40036e75094f1d06c4742c9d9d63858de
Victor Caruso <vic.caruso@gmail.com>
2022-05-06 12:44:20 -0700

    Wrap inputs in form and add event handler

--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -4,24 +4,31 @@ export default function Auth() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('Email', { email });
+  }
+
   return (
     <>
       <h1>Login</h1>
-      <label for="email">Email</label>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email address"
-      />
-      <label for="password">Password</label>
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      <button type="submit">Login</button>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email address"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
+      </form>
     </>
   );
 }
