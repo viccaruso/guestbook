b2e1b83ac17b47ac5bb197881067338915c561de
Victor Caruso <vic.caruso@gmail.com>
2022-05-06 12:13:16 -0700

    Set up inputs and state for Auth view component

--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Auth() {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <>
+      <h1>Login</h1>
+      <label for="email">Email</label>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email address"
+      />
+      <label for="password">Password</label>
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button type="submit">Login</button>
+    </>
+  );
 }
