d7b459aae9fceb58e24b16a7a040b6aac4bd51d7
Victor Caruso <vic.caruso@gmail.com>
2022-05-06 17:01:10 -0700

    Provide UserContext to all components rendered in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { Redirect } from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
+import { UserProvider } from './context/UserContext';
 import Auth from './views/Auth';
 import Guestbook from './views/Guestbook';
 
 export default function App() {
   return (
-    <Switch>
-      <Route path="/login">
-        <Auth />
-      </Route>
-      <PrivateRoute path="/guestbook">
-        <Guestbook />
-      </PrivateRoute>
-      <Route path="/">
-        <Redirect to="/guestbook" />
-      </Route>
-    </Switch>
+    <UserProvider>
+      <Switch>
+        <Route path="/login">
+          <Auth />
+        </Route>
+        <PrivateRoute path="/guestbook">
+          <Guestbook />
+        </PrivateRoute>
+        <Route path="/">
+          <Redirect to="/guestbook" />
+        </Route>
+      </Switch>
+    </UserProvider>
   );
 }
