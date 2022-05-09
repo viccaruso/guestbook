fe6624f4751925927c9635758a715a9c2e926a6f
Victor Caruso <vic.caruso@gmail.com>
2022-05-09 11:32:47 -0700

    Import logout from context and add button to sign out

--- src/views/Guestbook.jsx
+++ src/views/Guestbook.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { useUser } from '../context/UserContext';
 
 export default function Guestbook() {
-  return <div>Guestbook</div>;
+  const { logout } = useUser();
+  return (
+    <h1>Guestbook</h1>
+    <button onClick={logout}>Sign out</button>
+  );
 }
