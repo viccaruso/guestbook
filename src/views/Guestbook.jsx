c5b7b1a30488740efdd01b9619eeafb591e55112
Victor Caruso <vic.caruso@gmail.com>
2022-05-09 11:44:23 -0700

    Fetch entries on load and render

--- src/views/Guestbook.jsx
+++ src/views/Guestbook.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useUser } from '../context/UserContext';
+import { getEntries } from '../services/entries';
 
 export default function Guestbook() {
   const { logout } = useUser();
+  const [entries, setEntries] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+      .then(() => getEntries())
+      .then((results) => setEntries(results))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <h1>Guestbook</h1>
-    <button onClick={logout}>Sign out</button>
+    <>
+      <h1>Guestbook</h1>
+      <button onClick={logout}>Sign out</button>
+      {loading ? (
+        <p>Loading guestbook entries...</p>
+      ) : (
+        <div>
+          {entries.map((entry, index) => (
+            <p key={index}>{entry.content}</p>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
