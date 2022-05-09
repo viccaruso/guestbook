7b3659e818c9d90fd8635b2e31d98845da2b416b
Victor Caruso <vic.caruso@gmail.com>
2022-05-09 14:36:27 -0700

    Test successfully logs in and loads guestbook entries using msw

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,14 +5,27 @@ import { rest } from 'msw';
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
 
-const server = setupServer();
+const server = setupServer(
+  rest.post(`${process.env.SUPABASE_API_URL}/auth/v1/token`, (req, res, ctx) =>
+    res(ctx.json({ user: { email: 'test@test.com' } }))
+  ),
+  rest.get(`${process.env.SUPABASE_API_URL}/rest/v1/entries`, (req, res, ctx) =>
+    res(
+      ctx.json([
+        { content: 'Hello, world!' },
+        { content: 'Goodbye, world!' },
+        { content: "I'm back, world!" },
+      ])
+    )
+  )
+);
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe('<App />', () => {
-  it('renders a list of guestbook entries', async () => {
+  it('Should log a user in and redirect to Guestbook page', async () => {
     render(
       <MemoryRouter>
         <App />
@@ -28,8 +41,12 @@ describe('<App />', () => {
     const loginButton = screen.getByRole('button', { name: /Login/ });
     userEvent.click(loginButton);
 
-    const guestbookHeading = await screen.findByRole('heading', {
+    await screen.findByRole('heading', {
       name: /Guestbook/,
     });
+
+    await screen.findByText('Hello, world!');
+    await screen.findByText('Goodbye, world!');
+    await screen.findByText("I'm back, world!");
   });
 });
